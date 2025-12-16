7e5de4dca8e1efa32d8f13a32d84099985adfad4
JosmarHerrera <josmarcal58@gmail.com>
2025-12-16 06:44:02 -0600

    Update TipoProductoService.js

--- src/Service/TipoProductoService.js
+++ src/Service/TipoProductoService.js
@@ -1,29 +1,19 @@
 // src/services/TipoProductoService.js
-import { API_TIPO, API_PRODUCTO, apiFetch } from "../Service/apiHelper";
+import { API_TIPO, apiFetch } from "../Service/apiHelper";
 
-export const listarTipos = async () => {
-  return apiFetch(API_TIPO);
-};
+export const listarTipos = () => apiFetch(`${API_TIPO}`, { method: "GET" });
 
-export const crearTipo = async (tipo) => {
-  return apiFetch(API_TIPO, {
+export const crearTipo = (tipo) =>
+  apiFetch(`${API_TIPO}`, {
     method: "POST",
     body: JSON.stringify(tipo),
   });
-};
 
-export const actualizarTipo = async (id, tipo) => {
-  return apiFetch(`${API_TIPO}/${id}`, {
+export const actualizarTipo = (id, tipo) =>
+  apiFetch(`${API_TIPO}/${id}`, {
     method: "PUT",
     body: JSON.stringify(tipo),
   });
-};
 
-export const eliminarTipo = async (id) => {
-  return apiFetch(`${API_TIPO}/${id}`, { method: "DELETE" });
-};
-
-// (si en algún lado usas API_PRODUCTO desde aquí)
-export const listarProductos = async () => {
-  return apiFetch(API_PRODUCTO);
-};
+export const eliminarTipo = (id) =>
+  apiFetch(`${API_TIPO}/${id}`, { method: "DELETE" });
